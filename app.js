c928f257644b996d46fa762036ed67bce4fc4db4
John Lee <jy.johnlee@gmail.com>
2014-08-01 03:32:13 -0700

    init

---
+++ app.js
@@ -0,0 +1,194 @@
+PSD = Framer.Importer.load("imported/firstMock")
+
+dimmingView = PSD.dimmingView;
+
+resumeButton = PSD.resumeButton;
+guitarButton = PSD.guitarButton;
+gameButton = PSD.gameButton;
+blogButton = PSD.blogButton;
+
+internetHighlighted = PSD.internetHighlighted;
+emailHighlighted = PSD.emailHighlighted;
+phoneHighlighted = PSD.phoneHighlighted;
+twitterHighlighted = PSD.twitterHighlighted;
+
+resume = PSD.Resume;
+fullResume = PSD.fullResumeButton;
+exitResume = PSD.Exit;
+
+notReady = new Layer({
+  x: 0,
+  y: 0,
+  width: 350,
+  height: 180,
+  scale: 0,
+  borderRadius: "8px",
+  backgroundColor: "#28acff",
+  color: "white"
+});
+notReady.style = {
+  "font-size": "30px",
+  "text-align": "center",
+  "line-height": "60px"
+}
+notReady.html = 'Resume is not ready yet :(... An amazing one coming soon';
+notReady.center()
+
+resume.scale = 0.0;
+dimmingView.opacity = 0.0;
+internetHighlighted.opacity = 0.0;
+emailHighlighted.opacity = 0.0;
+phoneHighlighted.opacity = 0.0;
+twitterHighlighted.opacity = 0.0;
+
+curveButton = "spring(150,100,40)"
+
+resumeButton.on(Events.MouseOver, function() {
+  return resumeButton.animate({
+    properties: {
+      scale: 1.2
+    },
+    curve: curveButton
+  });
+});
+resumeButton.on(Events.MouseOut, function() {
+  return resumeButton.animate({
+    properties: {
+      scale: 1
+    },
+    curve: curveButton
+  });
+});
+resumeButton.on(Events.Click, function() {
+  dimmingView.opacity = 1.0;
+  resume.animate({
+    properties: {
+      scale: 1
+    },
+    curve: 'spring',
+    curveOptions: {
+      friction: 40
+    }
+  });
+});
+fullResume.on(Events.Click, function() {
+  popUp = notReady.animate({
+    properties:{
+      scale: 1
+    },
+    curve:'spring',
+    curveOptions: {
+      friciton: 40
+    }
+  });
+  return popUp.on('end', function() {
+    return notReady.animate({
+      properties: {
+        scale: 0
+      },
+      curve: 'cubic-bezier',
+      time: 1
+    });
+  });
+});
+exitResume.on(Events.Click, function() {
+  dimmingView.opacity = 0.0;
+  resume.animate({
+    properties: {
+      scale: 0
+    },
+    curve: 'spring',
+    curveOptions: {
+      friction: 40
+    }
+  });
+});
+
+
+guitarButton.on(Events.MouseOver, function() {
+  return guitarButton.animate({
+    properties: {
+      scale: 1.2
+    },
+    curve: curveButton
+  });
+});
+guitarButton.on(Events.MouseOut, function() {
+  return guitarButton.animate({
+    properties: {
+      scale: 1
+    },
+    curve: curveButton
+  });
+});
+guitarButton.on(Events.Click, function() {
+  var link = document.getElementById("guitar");
+  link.click()
+});
+gameButton.on(Events.MouseOver, function() {
+  return gameButton.animate({
+    properties: {
+      scale: 1.2
+    },
+    curve: curveButton
+  });
+});
+gameButton.on(Events.MouseOut, function() {
+  return gameButton.animate({
+    properties: {
+      scale: 1
+    },
+    curve: curveButton
+  });
+});
+gameButton.on(Events.Click, function() {
+  var link = document.getElementById("tetris");
+  link.click()
+});
+
+blogButton.on(Events.MouseOver, function() {
+  return blogButton.animate({
+    properties: {
+      scale: 1.2
+    },
+    curve: curveButton
+  });
+});
+blogButton.on(Events.MouseOut, function() {
+  return blogButton.animate({
+    properties: {
+      scale: 1
+    },
+    curve: curveButton
+  });
+});
+
+internetHighlighted.on(Events.MouseOver, function() {
+  internetHighlighted.opacity = 1.0;
+});
+internetHighlighted.on(Events.MouseOut, function() {
+  internetHighlighted.opacity = 0.0;
+});
+emailHighlighted.on(Events.MouseOver, function() {
+  emailHighlighted.opacity = 1.0;
+});
+emailHighlighted.on(Events.MouseOut, function() {
+  emailHighlighted.opacity = 0.0;
+});
+phoneHighlighted.on(Events.MouseOver, function() {
+  phoneHighlighted.opacity = 1.0;
+});
+phoneHighlighted.on(Events.MouseOut, function() {
+  phoneHighlighted.opacity = 0.0;
+});
+twitterHighlighted.on(Events.MouseOver, function() {
+  twitterHighlighted.opacity = 1.0;
+});
+twitterHighlighted.on(Events.MouseOut, function() {
+  twitterHighlighted.opacity = 0.0;
+});
+twitterHighlighted.on(Events.Click, function() {
+  var link = document.createElement("a");
+  link.setAttribute("href", "https://twitter.com/jjojohjohn");
+  link.click();
+});
---
+++ imported/firstMock/layers.json.js
@@ -0,0 +1,502 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["firstMock/layers.json.js"] = [
+	{
+		"id": 102,
+		"name": "Background",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Background.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 640,
+				"height": 1136
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1606861185"
+	},
+	{
+		"id": 88,
+		"name": "main pictures",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/main pictures.png",
+			"frame": {
+				"x": 77,
+				"y": 236,
+				"width": 488,
+				"height": 186
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "555030501"
+	},
+	{
+		"id": 61,
+		"name": "border",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/border.png",
+			"frame": {
+				"x": 0,
+				"y": 164,
+				"width": 640,
+				"height": 864
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1546461047"
+	},
+	{
+		"id": 164,
+		"name": "dimmingView",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/dimmingView.png",
+			"frame": {
+				"x": 0,
+				"y": 0,
+				"width": 640,
+				"height": 1136
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1626826071"
+	},
+	{
+		"id": 84,
+		"name": "text",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/text.png",
+			"frame": {
+				"x": 75,
+				"y": 48,
+				"width": 472,
+				"height": 460
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1413792651"
+	},
+	{
+		"id": 104,
+		"name": "guitarButton",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/guitarButton.png",
+			"frame": {
+				"x": 193,
+				"y": 607,
+				"width": 100,
+				"height": 100
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1212753385"
+	},
+	{
+		"id": 106,
+		"name": "gameButton",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/gameButton.png",
+			"frame": {
+				"x": 349,
+				"y": 607,
+				"width": 100,
+				"height": 100
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1916377559"
+	},
+	{
+		"id": 108,
+		"name": "blogButton",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/blogButton.png",
+			"frame": {
+				"x": 513,
+				"y": 607,
+				"width": 100,
+				"height": 100
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1215264612"
+	},
+	{
+		"id": 110,
+		"name": "resumeButton",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/resumeButton.png",
+			"frame": {
+				"x": 29,
+				"y": 607,
+				"width": 100,
+				"height": 100
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "73431212"
+	},
+	{
+		"id": 118,
+		"name": "internet",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/internet.png",
+			"frame": {
+				"x": 49,
+				"y": 759,
+				"width": 386,
+				"height": 60
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1528390166"
+	},
+	{
+		"id": 150,
+		"name": "internetHighlighted",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/internetHighlighted.png",
+			"frame": {
+				"x": 49,
+				"y": 759,
+				"width": 386,
+				"height": 60
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1354315745"
+	},
+	{
+		"id": 112,
+		"name": "email",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/email.png",
+			"frame": {
+				"x": 49,
+				"y": 856,
+				"width": 427,
+				"height": 60
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1845413433"
+	},
+	{
+		"id": 145,
+		"name": "emailHighlighted",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/emailHighlighted.png",
+			"frame": {
+				"x": 49,
+				"y": 856,
+				"width": 427,
+				"height": 60
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1162390507"
+	},
+	{
+		"id": 114,
+		"name": "phone",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/phone.png",
+			"frame": {
+				"x": 49,
+				"y": 948,
+				"width": 362,
+				"height": 60
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "325788487"
+	},
+	{
+		"id": 136,
+		"name": "phoneHighlighted",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/phoneHighlighted.png",
+			"frame": {
+				"x": 49,
+				"y": 948,
+				"width": 362,
+				"height": 60
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1285280103"
+	},
+	{
+		"id": 116,
+		"name": "twitter",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/twitter.png",
+			"frame": {
+				"x": 49,
+				"y": 1046,
+				"width": 342,
+				"height": 60
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1103617781"
+	},
+	{
+		"id": 135,
+		"name": "twitterHighlighted",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/twitterHighlighted.png",
+			"frame": {
+				"x": 49,
+				"y": 1046,
+				"width": 342,
+				"height": 60
+			}
+		},
+		"imageType": "png",
+		"children": [
+			
+		],
+		"modification": "1760804114"
+	},
+	{
+		"id": 151,
+		"name": "Resume",
+		"layerFrame": {
+			"x": 0,
+			"y": 0,
+			"width": 640,
+			"height": 1136
+		},
+		"maskFrame": null,
+		"image": {
+			"path": "images/Resume.png",
+			"frame": {
+				"x": 20,
+				"y": 68,
+				"width": 600,
+				"height": 1000
+			}
+		},
+		"imageType": "png",
+		"children": [
+			{
+				"id": 167,
+				"name": "Exit",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1136
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/Exit.png",
+					"frame": {
+						"x": 561,
+						"y": 88,
+						"width": 37,
+						"height": 42
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "1674578399"
+			},
+			{
+				"id": 162,
+				"name": "fullResumeButton",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 640,
+					"height": 1136
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/fullResumeButton.png",
+					"frame": {
+						"x": 198,
+						"y": 85,
+						"width": 246,
+						"height": 65
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "1818672556"
+			}
+		],
+		"modification": "1495334600"
+	}
+]
